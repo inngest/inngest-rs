13d64c26eec8172c6abd0ebe54ea32d347c798af
Darwin D Wu <darwin67@users.noreply.github.com>
2023-08-09 00:29:44 -0700

    Add dummy http handlers for register and invoke

--- src/bin/dev.rs
+++ src/bin/dev.rs
@@ -1,13 +1,26 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, put},
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/api/inngest", put(register).post(invoke));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn register() -> &'static str {
+    "Register"
+}
+
+async fn invoke() -> &'static str {
+    "Invoke"
 }
