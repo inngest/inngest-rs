5b4bbccb64f363e4f4e5f9f769f6d7f11c760dd7
Darwin D Wu <darwin67@users.noreply.github.com>
2023-08-10 00:48:17 -0700

    Add function related structs

--- src/bin/dev.rs
+++ src/bin/dev.rs
@@ -18,7 +18,39 @@ struct App {
 }
 
 #[derive(Clone, Deserialize, Serialize)]
-struct Function {}
+struct Trigger {
+    event: Option<String>,
+    expression: Option<String>,
+    cron: Option<String>,
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+struct Function {
+    id: String,
+    name: String,
+    triggers: Vec<Trigger>,
+    steps: Vec<FunctionStep>,
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+struct FunctionStep {
+    id: String,
+    name: String,
+    run_time: FunctionStepRuntime,
+    retries: FunctionStepRetry,
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+struct FunctionStepRetry {
+    attempts: u8,
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+struct FunctionStepRuntime {
+    url: String,
+    #[serde(rename = "type")]
+    method: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -36,6 +68,13 @@ async fn main() {
 
 async fn register() -> Result<(), String> {
     let client = reqwest::Client::new();
+    let func = Function {
+        id: "dummy-func".to_string(),
+        name: "Dummy func".to_string(),
+        triggers: vec![],
+        steps: vec![],
+    };
+
     let payload = App {
         app_name: "InngestApp".to_string(),
         deploy_type: "ping".to_string(),
@@ -43,7 +82,7 @@ async fn register() -> Result<(), String> {
         v: "1".to_string(),
         sdk: "rust:v0.0.1".to_string(),
         framework: "axum".to_string(),
-        functions: vec![],
+        functions: vec![func],
     };
 
     match client
