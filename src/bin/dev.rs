b8e184e6b144f671d85048e6d7e179e2bc28e890
Darwin <5746693+darwin67@users.noreply.github.com>
2023-08-10 23:20:18 -0700

    Register functions (#13)
    * Add function related structs
    * add debug and to the app body
    * register dummy functions and steps
    
    ---------
    Co-authored-by: Darwin D Wu <darwin67@users.noreply.github.com>

--- src/bin/dev.rs
+++ src/bin/dev.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
+
 use axum::{
     routing::{get, put},
     Router,
 };
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 struct App {
     #[serde(rename = "appName")]
     app_name: String,
@@ -17,8 +20,40 @@ struct App {
     functions: Vec<Function>,
 }
 
-#[derive(Clone, Deserialize, Serialize)]
-struct Function {}
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct Trigger {
+    event: Option<String>,
+    expression: Option<String>,
+    // cron: Option<String>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct Function {
+    id: String,
+    name: String,
+    triggers: Vec<Trigger>,
+    steps: HashMap<String, FunctionStep>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct FunctionStep {
+    id: String,
+    name: String,
+    runtime: FunctionStepRuntime,
+    retries: FunctionStepRetry,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct FunctionStepRetry {
+    attempts: u8,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct FunctionStepRuntime {
+    url: String,
+    #[serde(rename = "type")]
+    method: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -36,6 +71,31 @@ async fn main() {
 
 async fn register() -> Result<(), String> {
     let client = reqwest::Client::new();
+    let mut steps = HashMap::new();
+    steps.insert(
+        "step".to_string(),
+        FunctionStep {
+            id: "step".to_string(),
+            name: "step".to_string(),
+            runtime: FunctionStepRuntime {
+                url: "http://127.0.0.1:3000/api/inngest?fnId=dummy-func&step=step".to_string(),
+                method: "http".to_string(),
+            },
+            retries: FunctionStepRetry { attempts: 3 },
+        },
+    );
+
+    let func = Function {
+        id: "dummy-func".to_string(),
+        name: "Dummy func".to_string(),
+        triggers: vec![Trigger {
+            event: Some("test/event".to_string()),
+            expression: None,
+            // cron: None,
+        }],
+        steps,
+    };
+
     let payload = App {
         app_name: "InngestApp".to_string(),
         deploy_type: "ping".to_string(),
@@ -43,11 +103,13 @@ async fn register() -> Result<(), String> {
         v: "1".to_string(),
         sdk: "rust:v0.0.1".to_string(),
         framework: "axum".to_string(),
-        functions: vec![],
+        functions: vec![func],
     };
 
+    println!("Payload: {:#?}", json!(payload));
+
     match client
-        .post("http://127.0.0.1:8288")
+        .post("http://127.0.0.1:8288/fn/register")
         .json(&payload)
         .send()
         .await
