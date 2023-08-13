0483a7b17012263af298b9527a991e96c7da477f
Darwin D Wu <darwin67@users.noreply.github.com>
2023-08-13 14:21:06 -0700

    get registration to work with create_function

--- src/bin/dev.rs
+++ src/bin/dev.rs
@@ -1,20 +1,50 @@
+use std::sync::Arc;
+
 use axum::{
     routing::{get, put},
     Router,
 };
 
-use inngest::router::axum as inngest_axum;
+use inngest::{
+    function::{create_function, FunctionOps, Input, ServableFunction, Trigger},
+    router::{axum as inngest_axum, Handler},
+};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    let mut inngest_handler = Handler::new();
+    inngest_handler.register_fn(dummy_fn());
+
+    let inngest_state = Arc::new(inngest_handler);
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/api/inngest", put(inngest_axum::register));
+        .route("/api/inngest", put(inngest_axum::register))
+        .with_state(inngest_state);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+struct DummyEvent {}
+
+fn dummy_fn() -> impl ServableFunction {
+    create_function(
+        FunctionOps {
+            name: "Dummy func".to_string(),
+            ..Default::default()
+        },
+        Trigger::EventTrigger {
+            event: "test/event".to_string(),
+            expression: None,
+        },
+        |_input: Input<DummyEvent>| {
+            println!("In dummy function");
+
+            Ok(Box::new(()))
+        },
+    )
 }
