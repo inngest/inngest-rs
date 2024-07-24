34dc180f6d7115a43af91573365921ff320c9c11
Darwin D Wu <darwin67@users.noreply.github.com>
2024-07-24 10:27:27 -0700

    split base url to api and event api

--- inngest/src/inngest.rs
+++ inngest/src/inngest.rs
@@ -1,6 +1,7 @@
 pub struct Inngest {
     pub app_id: String,
-    pub base_url: Option<String>,
+    pub api_base_url: Option<String>,
+    pub event_api_base_url: Option<String>,
     pub event_key: Option<String>,
     pub env: Option<String>,
     pub is_dev: Option<bool>,
@@ -10,7 +11,8 @@ impl Inngest {
     pub fn new(app_id: String) -> Self {
         Inngest {
             app_id,
-            base_url: None,
+            api_base_url: None,
+            event_api_base_url: None,
             event_key: None,
             env: None,
             is_dev: None,
