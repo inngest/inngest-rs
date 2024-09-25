98254cddc42df529f8101c727c9bc59ca2cd2619
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2024-09-25 15:06:14 -0700

    wip

--- inngest/src/handler.rs
+++ inngest/src/handler.rs
@@ -107,9 +107,6 @@ impl<T, E> Handler<T, E> {
         T: for<'de> Deserialize<'de> + Debug,
         E: Into<InngestError>,
     {
-        println!("running function: {}", query.fn_id);
-        println!("body: {:#?}", body);
-
         let data = match serde_json::from_value::<RunRequestBody<T>>(body.clone()) {
             Ok(res) => res,
             Err(err) => {
