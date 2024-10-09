a00987cd5ae0ec7d0890049d8633354857281057
Darwin D Wu <darwin67@users.noreply.github.com>
2024-10-09 00:25:46 -0700

    add headers for errors and set retry after error data correctly

--- inngest/src/header.rs
+++ inngest/src/header.rs
@@ -5,7 +5,7 @@ use crate::handler::Kind;
 pub(crate) const CONTENT_TYPE: &str = "content-type";
 pub(crate) const RETRY_AFTER: &str = "retry-after";
 pub(crate) const SERVER_TIMING: &str = "server-timing";
-pub(crate) const USE_AGENT: &str = "user-agent";
+pub(crate) const USER_AGENT: &str = "user-agent";
 
 // Inngest specific ones
 pub(crate) const INNGEST_ENV: &str = "x-inngest-env";
